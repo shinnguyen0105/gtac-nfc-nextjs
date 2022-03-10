7346f0a786ca7e6220f928f883e8d36bd0eee34a
Nhat Nguyen <nhatnc015@gmail.com>
2022-03-10 23:50:58 +0700

    fix the vercel build failed

--- pages/[mid].js
+++ pages/[mid].js
@@ -28,24 +28,24 @@ const Member = ({ member }) => {
 								viewBox='0 0 64 64'
 							>
 								<path
-									stroke-width='5'
-									stroke-miterlimit='10'
+									strokeWidth='5'
+									strokeMiterlimit='10'
 									stroke='#15803c'
 									fill='none'
 									d='M55.9 28.3c.1-.8.1-1.5.1-2.3a24 24 0 0 0-48 0c0 .8 0 1.6.1 2.3v.3C10.1 47.6 32 61 32 61s21.9-13.6 23.8-32.3z'
 									data-name='layer2'
-									stroke-linejoin='round'
-									stroke-linecap='round'
+									strokeLinejoin='round'
+									strokeLinecap='round'
 								></path>
 								<path
 									d='M32 14v14h10'
-									stroke-width='5'
-									stroke-miterlimit='10'
+									strokeWidth='5'
+									strokeMiterlimit='10'
 									stroke='#15803c'
 									fill='none'
 									data-name='layer1'
-									stroke-linejoin='round'
-									stroke-linecap='round'
+									strokeLinejoin='round'
+									strokeLinecap='round'
 								></path>
 							</svg>{' '}
 							{member.dateOfBirth}
@@ -85,33 +85,33 @@ const Member = ({ member }) => {
 							>
 								<path
 									d='M54 33.6V2H10v31.6M18 12h10m-4 8h22m-28 8h28'
-									stroke-width='5'
-									stroke-miterlimit='10'
+									strokeWidth='5'
+									strokeMiterlimit='10'
 									stroke='#15803c'
 									fill='none'
 									data-name='layer2'
-									stroke-linejoin='round'
-									stroke-linecap='round'
+									strokeLinejoin='round'
+									strokeLinecap='round'
 								></path>
 								<path
 									d='M42.08 41.944L62 28M2 28l19.92 13.944M2 62l20.929-21.071c3.905-3.9 14.237-3.9 18.143 0L62 62'
-									stroke-width='5'
-									stroke-miterlimit='10'
+									strokeWidth='5'
+									strokeMiterlimit='10'
 									stroke='#15803c'
 									fill='none'
 									data-name='layer1'
-									stroke-linejoin='round'
-									stroke-linecap='round'
+									strokeLinejoin='round'
+									strokeLinecap='round'
 								></path>
 								<path
 									d='M54 21.063L62 28v34H2V28l8-6.937'
-									stroke-width='5'
-									stroke-miterlimit='10'
+									strokeWidth='5'
+									strokeMiterlimit='10'
 									stroke='#15803c'
 									fill='none'
 									data-name='layer1'
-									stroke-linejoin='round'
-									stroke-linecap='round'
+									strokeLinejoin='round'
+									strokeLinecap='round'
 								></path>
 							</svg>{' '}
 							{member.email}
