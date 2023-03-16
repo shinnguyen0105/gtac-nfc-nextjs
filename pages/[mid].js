ab51089f060b6f67be6f6bc4525892e2aea18771
Greenwich Teaching Assistant Club <102477438+gtac-uog@users.noreply.github.com>
2023-03-16 09:11:56 +0700

    Update [mid].js

--- pages/[mid].js
+++ pages/[mid].js
@@ -405,15 +405,7 @@ const Member = ({ member }) => {
 						{/* back to homepage */}
 						<div
 							id='profile'
-							className='rounded-3xl shadow-2xl mx-auto mt-10'
-						>
-							<div className='flex justify-center'>
-								<Link href='/'>
-									<button className='bg-yellow-400 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded'>
-										Back to Homepage
-									</button>
-								</Link>
-							</div>
+							className='rounded-3xl shadow-2xl mx-auto mt-10'>
 						</div>
 						<div className='mt-5 text-center text-slate-400'>
 							© Copyright owned by Greenwich Teaching Assistant Club - Danang
