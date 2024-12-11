09d3f444b4ed1f1a954e76771f4a0fcbf534e39e
gtac-uog <gtac.uog@gmail.com>
2024-12-11 11:52:32 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,23 +36,22 @@ const Members = () => {
 					{searchTerm ? (
 						<div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5'>
 							{filteredMembers.map((member) => (
-								<Link key={member.id} href='/[mid]' as={`/${member.id}`}>
-									<div
-										key={member.id}
-										className='rounded overflow-hidden bg-inherit hover:shadow-2xl'
-									>
-										<img
-											className='w-full'
-											src={`images/${member.id}.png`}
-											alt={`${member.name}`}
-										/>
-										<div className='font-bold text-sm mb-2 text-white'>
-											{member.name}
-										</div>
-										<p className='text-slate-200 text-xs'>Phone: {member.phone}</p>
-										<p className='text-slate-200 text-xs'>Email: {member.email}</p>
-									</div>
-								</Link>
+								<Link key={member.id} href='/[mid]' as={`/${member.id}`} passHref>
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									className="rounded overflow-hidden bg-inherit hover:shadow-2xl"
+								>
+									<img
+										className="w-full"
+										src={`images/${member.id}.png`}
+										alt={`${member.name}`}
+									/>
+									<div className="font-bold text-sm mb-2 text-white">{member.name}</div>
+									<p className="text-slate-200 text-xs">Phone: {member.phone}</p>
+									<p className="text-slate-200 text-xs">Email: {member.email}</p>
+								</a>
+							</Link>
 							))}
 						</div>
 					) : (
